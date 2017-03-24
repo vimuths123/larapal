d912cdbd950139e22781f9cc7022fab72ca3f823
vimuths123 <vimuths@yahoo.com>
2017-03-24 18:59:58 +0530

    Update LarapalController.php

--- src/Http/LarapalController.php
+++ src/Http/LarapalController.php
@@ -1,1 +1,11 @@
 
+<?php
+namespace vimuths123\larapal\Http;
+
+use App\Http\Controllers\Controller;
+
+class LarapalController extends Controller {
+    public function viewPaypal() {
+        return view('larapal::paypal');
+    }
+}
